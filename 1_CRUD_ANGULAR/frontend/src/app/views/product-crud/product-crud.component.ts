63b9a43bd6c5f08536b53c120aac774661d5bf1d
R <rodrigoraccuia@unesc.net>
2022-06-29 16:47:38 -0300

    added routing

---
+++ 1_CRUD_ANGULAR/frontend/src/app/views/product-crud/product-crud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-crud',
+  templateUrl: 'product-crud.html',
+  styleUrls: ['./product-crud.css']
+})
+export class ProductCrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
