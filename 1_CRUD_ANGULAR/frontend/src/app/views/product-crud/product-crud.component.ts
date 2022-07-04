54bd01ce3f110261f426c26251f8f4804d2f6aae
R <rodrigoraccuia@unesc.net>
2022-07-04 17:01:00 -0300

    get products

--- 1_CRUD_ANGULAR/frontend/src/app/views/product-crud/product-crud.component.ts
+++ 1_CRUD_ANGULAR/frontend/src/app/views/product-crud/product-crud.component.ts
@@ -3,8 +3,8 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-crud',
-  templateUrl: 'product-crud.html',
-  styleUrls: ['./product-crud.css']
+  templateUrl: 'product-crud.component.html',
+  styleUrls: ['./product-crud.component.css']
 })
 export class ProductCrudComponent implements OnInit {
 
